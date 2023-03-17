bb41147f7140fb79df9d83069a65327229c16d17
Yvkibira <122603167+Yvkibira@users.noreply.github.com>
2023-03-18 01:16:01 +0300

    Update web.js

--- web.js
+++ web.js
@@ -10,3 +10,17 @@ toggleButton.addEventListener('click', function() {
 
 
 
+// Get references to the button and the div
+// Get references to the button and the div
+const myButton = document.getElementById('myButton');
+const myDiv = document.getElementById('myDiv');
+
+// Add a click event listener to the button
+myButton.addEventListener('click', function() {
+  // Toggle the visibility of the div
+  if (myDiv.style.display === 'none') {
+    myDiv.style.display = 'block';
+  } else {
+    myDiv.style.display = 'none';
+  }
+});
