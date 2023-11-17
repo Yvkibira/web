7aa638f5f9a04a82a6156b4061f8751e50f0f566
Yvkibira <122603167+Yvkibira@users.noreply.github.com>
2023-11-17 17:58:15 +0300

    Update web.js

--- web.js
+++ web.js
@@ -1,3 +1,15 @@
-const toggleButton = document.getElementById('toggle-button');const toggleElements = document.querySelectorAll('.toggle-element');toggleButton.addEventListener('click', function() {toggleElements.forEach(element => {element.classList.toggle('hidden');});});
-window.onload = function() {const toggleButton = document.getElementById('toggle-button');const toggleElements = document.querySelectorAll('.toggle-element');toggleButton.addEventListener('click', function() {toggleElements.forEach(element => {element.classList.toggle('hidden');});});
-var scriptElement = document.createElement("script");scriptElement.async = true;scriptElement.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2705647526062186";scriptElement.crossOrigin = "anonymous";var head = document.querySelector("head");head.appendChild(scriptElement);
+window.onload = function() {
+    const toggleButton = document.getElementById('toggle-button');
+    const toggleElements = document.querySelectorAll('.toggle-element');
+    toggleButton.addEventListener('click', function() {
+        toggleElements.forEach(element => {
+            element.classList.toggle('hidden');
+        });
+    });
+    var scriptElement = document.createElement("script");
+    scriptElement.async = true;
+    scriptElement.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2705647526062186";
+    scriptElement.crossOrigin = "anonymous";
+    var head = document.querySelector("head");
+    head.appendChild(scriptElement);
+};
