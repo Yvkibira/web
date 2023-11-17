9202ff9785c1161d3b1dc593d9a7cba78f7ffe16
Yvkibira <122603167+Yvkibira@users.noreply.github.com>
2023-11-17 18:24:02 +0300

    Update web.js

--- web.js
+++ web.js
@@ -6,10 +6,10 @@ window.onload = function() {
             element.classList.toggle('hidden');
         });
     });
-    var scriptElement = document.createElement("script");
+};
+  var scriptElement = document.createElement("script");
     scriptElement.async = true;
     scriptElement.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2705647526062186";
     scriptElement.crossOrigin = "anonymous";
     var head = document.querySelector("head");
     head.appendChild(scriptElement);
-};
